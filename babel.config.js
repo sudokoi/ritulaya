c8fd6af12f5ff9d0c3f9de24bafac4b3a7d51b49
Sudhanshu Ranjan <perfectsudh@gmail.com>
2026-08-14 13:04:19 +0530

    fix: resolve SQL migration imports in Metro bundling
    Metro could not resolve the .sql migration files imported by drizzle's migrations.js. Adds babel-plugin-inline-import (with .sql) and pushes 'sql' onto Metro sourceExts so the migration SQL is inlined as a string.

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      ["inline-import", { extensions: [".sql"] }],
+      "react-native-reanimated/plugin",
+    ],
   }
 }
--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,8 @@ const { withNativeWind } = require("nativewind/metro")
 
 const config = getDefaultConfig(__dirname)
 
+config.resolver.sourceExts.push("sql")
+
 module.exports = withNativeWind(config, {
   input: "./src/global.css",
   inlineRem: 14,
